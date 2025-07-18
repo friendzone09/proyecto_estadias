12dfca09eafe67a0d938fd41f9763d14cace2565
friendzone09 <alejandroperez9474@gmail.com>
2025-07-17 18:49:20 -0600

    version 1.8.20

--- front/src/globalComponents/footer.jsx
+++ front/src/globalComponents/footer.jsx
@@ -1,27 +1,57 @@
 import { useUser } from "../contexts/userContext/UserContext"
 import { Link } from "react-router-dom"
-import { Mail, MapPinned, MessageSquareText, BookOpen } from "lucide-react"
+import { Mail, MapPinned, MessageSquareText, BookOpen} from "lucide-react"
+import { AiFillTikTok } from "react-icons/ai";
+import { FaSquareFacebook, FaInstagram } from "react-icons/fa6";
 
-function Footer(){
+function Footer() {
 
-    const {user} = useUser()
+    const { user } = useUser()
 
-    if(!user) return null
+    if (!user) return null
 
-    return(
+    return (
         <footer>
-            <a 
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=psicologiaghamaris@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            > <Mail/> Enviar un correo </a>
-            <a href="https://maps.app.goo.gl/dkJabJMrfmq7S8588"
-            target="_blank"
-            > <MapPinned/>¿Dónde nos encuentras?</a>
-            <a href="https://wa.me/+522412231708"
-            target="_blank"
-            > <MessageSquareText /> Whatsapp</a>
-            <Link to={'/how-use'}><BookOpen/> ¿Cómo se usa?</Link>
+
+            <div>
+                <span>Contactanos</span>
+                <div className="footer--links">
+                    <a
+                        href="https://mail.google.com/mail/?view=cm&fs=1&to=psicologiaghamaris@gmail.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    > <Mail /> Enviar un correo </a>
+
+                    <a href="https://wa.me/+522412231708"
+                        target="_blank"
+                    > <MessageSquareText /> Whatsapp</a>
+                </div>
+            </div>
+
+            <div>
+                <span>Direccion</span>
+                <div className="footer--links">
+                    <a href="https://maps.app.goo.gl/dkJabJMrfmq7S8588"
+                        target="_blank"
+                    > <MapPinned />¿Dónde nos encuentras?</a>
+                </div>
+            </div>
+
+            <div>
+                <span>Manual</span>
+                <div className="footer--links">
+                    <Link to={'/how-use'}><BookOpen /> ¿Cómo se usa?</Link>
+                </div>
+            </div>
+
+            <div>
+                <span>Nuestras redes</span>
+                <div className="footer--social">
+                    <a href="https://www.facebook.com/share/1EkDtyfwba/"><FaSquareFacebook size={40}/></a>
+                    <a href="https://www.instagram.com/psicologiaghamaris?igsh=MXBjZGtuMnBxOXJjaw=="><FaInstagram size={40} /></a>
+                    <a href="https://www.tiktok.com/@psicologia_ghamaris?_t=ZS-8y7ILXErtqy&_r=1"><AiFillTikTok size={40}/></a>
+                </div>
+            </div>
         </footer>
     )
 }
