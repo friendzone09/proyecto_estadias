52489f9848cbc482a3886c19f7586d15d7f2d9c4
friendzone09 <alejandroperez9474@gmail.com>
2025-07-25 12:31:41 -0600

    version 1.9.1

--- back/app/models/db.py
+++ back/app/models/db.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 def get_db_connection():
     try:
-        #conn = psycopg2.connect(os.getenv('DB_URL'))
-        conn=psycopg2.connect(host=os.getenv('DB_HOST'),
-                              dbname=os.getenv('DB_NAME'),
-                              port=os.getenv('DB_PORT'),
-                              user = os.getenv('DB_USER'),
-                              password =os.getenv('DB_PASSWORD'),
-                              )
+        conn = psycopg2.connect(os.getenv('DB_URL'))
+        # conn=psycopg2.connect(host=os.getenv('DB_HOST'),
+        #                       dbname=os.getenv('DB_NAME'),
+        #                       port=os.getenv('DB_PORT'),
+        #                       user = os.getenv('DB_USER'),
+        #                       password =os.getenv('DB_PASSWORD'),
+        #                       )
         return conn
     except psycopg2.Error as error:
         print(f"Base de datos no encontrada {error}")
